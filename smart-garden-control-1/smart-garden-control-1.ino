75e2063ebb1b63d8086b41926720b1894f3a742a
bulantech <bulantech@gmail.com>
2019-02-23 14:44:34 +0700

    add web server

--- smart-garden-control-1/smart-garden-control-1.ino
+++ smart-garden-control-1/smart-garden-control-1.ino
@@ -1,12 +1,24 @@
 #include <Ticker.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
 
 #define LED_BUILTIN D4
 #define RELAY   D8
 #define SENSOR  A0
 
+#ifndef APSSID
+#define APSSID "SmartGarden"
+#define APPSK  "thereisnospoon"
+#endif
+
 int sensorValue = 0;
+/* Set these to your desired credentials. */
+const char *ssid = APSSID;
+const char *password = APPSK;
 
 Ticker interval1Sec;
+ESP8266WebServer server(80);
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
@@ -18,10 +30,21 @@ void setup() {
   pinMode(RELAY, OUTPUT);
 
   interval1Sec.attach(1, readSensor);
+
+//  WiFi.softAP(ssid, password);
+  WiFi.softAP(ssid);
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
 // the loop function runs over and over again forever
 void loop() {
+  server.handleClient();
+  
 //  sensorValue = analogRead(SENSOR);
 //  Serial.println("sensorValue = " + String(sensorValue) );
 //  
@@ -40,6 +63,9 @@ void readSensor() {
   sensorValue = analogRead(SENSOR);
   Serial.println("sensorValue = " + String(sensorValue) );
   delay(50);
-  digitalWrite(LED_BUILTIN, HIGH);
- 
+  digitalWrite(LED_BUILTIN, HIGH); 
+}
+
+void handleRoot() {
+  server.send(200, "text/html", "<h1>You are connected</h1>");
 }
