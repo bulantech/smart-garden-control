1a5cf56762021f78a3a4fdffc278b45a6f1d3e62
Air <air-lidon@hotmail.com>
2019-02-23 11:09:56 +0700

    first commit

---
+++ smart-garden-control-1/smart-garden-control-1.ino
@@ -0,0 +1,24 @@
+#define LED_BUILTIN D4
+#define RELAY D8
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Start..");
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(RELAY, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(RELAY, HIGH);
+  Serial.println("Relay close");
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(RELAY, LOW);
+  Serial.println("Relay open");
+  delay(1000);                       // wait for a second
+}
