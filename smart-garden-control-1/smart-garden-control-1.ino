1198c243974e1fcce1c426a310ac076486ff6bac
bulantech <bulantech@gmail.com>
2019-02-23 14:05:58 +0700

    add ticker

--- smart-garden-control-1/smart-garden-control-1.ino
+++ smart-garden-control-1/smart-garden-control-1.ino
@@ -1,8 +1,12 @@
+#include <Ticker.h>
+
 #define LED_BUILTIN D4
 #define RELAY   D8
 #define SENSOR  A0
 
 int sensorValue = 0;
+
+Ticker interval1Sec;
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
@@ -12,19 +16,30 @@ void setup() {
   
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(RELAY, OUTPUT);
+
+  interval1Sec.attach(1, readSensor);
 }
 
 // the loop function runs over and over again forever
 void loop() {
+//  sensorValue = analogRead(SENSOR);
+//  Serial.println("sensorValue = " + String(sensorValue) );
+//  
+//  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+//  digitalWrite(RELAY, HIGH);
+//  Serial.println("Relay close");
+//  delay(1000);                       // wait for a second
+//  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+//  digitalWrite(RELAY, LOW);
+//  Serial.println("Relay open");
+//  delay(1000);                       // wait for a second
+}
+
+void readSensor() {
+  digitalWrite(LED_BUILTIN, LOW);
   sensorValue = analogRead(SENSOR);
   Serial.println("sensorValue = " + String(sensorValue) );
-  
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(RELAY, HIGH);
-  Serial.println("Relay close");
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(RELAY, LOW);
-  Serial.println("Relay open");
-  delay(1000);                       // wait for a second
+  delay(50);
+  digitalWrite(LED_BUILTIN, HIGH);
+ 
 }
